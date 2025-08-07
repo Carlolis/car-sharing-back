2f7b849e9c8768ed120a0e3f1086d50f8842ebc6
Carlolis <cilieff@gmail.com>
2025-08-07 18:24:05 +0200

    Update InvoiceEndpoints to fix path and clean up unused imports
    - Correct endpoint path from `/trips` to `/invoices`.
    - Remove unused imports (`UUID` and `domain.services`) for better clarity and maintainability.

--- web/src/scala/api/InvoiceEndpoints.scala
+++ web/src/scala/api/InvoiceEndpoints.scala
@@ -1,7 +1,6 @@
 package api
 
 import domain.models.invoice.*
-import domain.services
 import sttp.model.StatusCode
 import sttp.tapir.Endpoint
 import sttp.tapir.generic.auto.*
@@ -9,14 +8,12 @@ import sttp.tapir.json.zio.*
 import sttp.tapir.ztapir.*
 import zio.json.*
 
-import java.util.UUID
-
 case class ErrorResponse(message: String) derives JsonEncoder, JsonDecoder
 
 object InvoiceEndpoints:
   val createInvoice: Endpoint[Unit, (String, InvoiceCreate), (StatusCode, ErrorResponse), InvoiceId, Any] = endpoint
     .post
-    .in("api" / "trips")
+    .in("api" / "invoices")
     .in(auth.bearer[String]())
     .in(jsonBody[InvoiceCreate])
     .out(jsonBody[InvoiceId])
